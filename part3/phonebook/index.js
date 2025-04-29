7232886aa82aa41a02284208d92b356db3e3e358
l4n0r <giuseppe.trieste.97@gmail.com>
2025-04-29 11:59:29 +0000

    phonebook (exs 3.1-3.8)

---
+++ part3/phonebook/index.js
@@ -0,0 +1,74 @@
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+app.use(express.json())
+morgan.token('body', (req) => {
+	if (req.method === 'POST') return JSON.stringify(req.body)
+	return '';
+});
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+let persons = [
+	{
+		"id": "1",
+		"name": "Arto Hellas",
+		"number": "040-123456"
+	},
+	{
+		"id": "2",
+		"name": "Ada Lovelace",
+		"number": "39-44-5323523"
+	},
+	{
+		"id": "3",
+		"name": "Dan Abramov",
+		"number": "12-43-234345"
+	},
+	{
+		"id": "4",
+		"name": "Mary Poppendieck",
+		"number": "39-23-6423122"
+	}
+]
+
+app.get('/info', (request, response) => {
+	response.send(`Phonebook has info for ${persons.length} people<br />` + new Date().toLocaleString())
+})
+
+app.get('/api/persons', (request, response) => {
+	response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+	const id = request.params.id
+	const person = persons.find(person => person.id === id)
+
+	if (person) response.json(person)
+	else response.status(404).end()
+})
+
+app.post('/api/persons', (request, response) => {
+	const person = { ...request.body }
+
+	if (person.name == null || person.number == null) response.json({ error: 'invalid name or number'})
+	else if (persons.some(p => p.name === person.name)) response.json({ error: 'name must be unique'})
+	else {
+		person.id = String(Math.round(Math.random() * 10000))
+		persons = persons.concat(person)
+  
+		response.json(person)
+	}
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+	const id = request.params.id
+	persons = persons.filter(person => person.id !== id)
+
+	response.status(204).end()
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`)
+})
