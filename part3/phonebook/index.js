e711022e854e015b999926d7cd3ea87cc97fc342
l4n0r <giuseppe.trieste.97@gmail.com>
2025-04-29 12:26:26 +0000

    phonebook (exs 3.9), skipping 3.10-3.11

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -8,6 +8,7 @@ morgan.token('body', (req) => {
 	return '';
 });
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('dist'))
 
 let persons = [
 	{
