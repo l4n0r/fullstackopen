4da536b6451feae616f636ab7dc7730c04a9090c
l4n0r <giuseppe.trieste.97@gmail.com>
2025-06-02 21:16:47 +0000

    anecdotes (exs 7.4-7.6, skipped 7.7-7.8)

---
+++ part6/routed-anecdotes/src/hooks.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+export const useField = (type) => {
+  const [value, setValue] = useState("");
+
+  const onChange = (event) => setValue(event.target.value);
+  const reset = () => setValue("");
+
+  const inputProps = {
+    type,
+    value,
+    onChange,
+  };
+
+  return {
+    ...inputProps,
+    reset,
+    inputProps
+  };
+};
